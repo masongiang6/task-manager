2f7c1c15d1b645c0b49beb4e054f78dcf84a64a5
Mason Giang <masongiang6@gmail.com>
2023-09-21 17:27:29 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ mongoose.connection.on("connected", () => {
 })
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
+    app.use(express.static("client"))
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "index.html"))
